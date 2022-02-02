f3b0a1f7bb974f16121ace163a5b227cb53914a1
Joseph Anselm <iamjoeanselm@gmail.com>
2022-02-02 10:37:03 +0100

    backend fix

--- schemas/author.js
+++ schemas/author.js
@@ -14,6 +14,11 @@ export default {
       type: "string",
     },
     {
+      name: "linkedin",
+      title: "LinkedIn Url",
+      type: "string",
+    },
+    {
       name: "slug",
       title: "Slug",
       type: "slug",
